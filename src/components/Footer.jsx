d238651881841a4fc0847903a597f616b98e88ca
SharifHafizi <112721514+SharifHafizi@users.noreply.github.com>
2023-04-02 17:20:01 +0200

    StrictMood disabled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react";
 import style from "./Footer.module.css";
 import linkedin from "../assets/linked.png";
 import insta from "../assets/insta.png";
 import github from "../assets/github.png";
 function Footer() {
+  const [visitor, setVisitor] = useState();
+  useEffect(() => {
+    fetch("https://api.countapi.xyz/update/portfolio/kodehode/?amount= 1")
+      .then((res) => res.json())
+      .then((res) => {
+        setVisitor(res.value);
+      });
+  }, []);
   return (
     <footer className={style.mainCon}>
       <p> &#169; 2023 Sharif Hafizi. All Rights Reserved.</p>
@@ -12,7 +21,7 @@ function Footer() {
           href="https://www.linkedin.com/in/sharif-hafizi-21370b26b/"
           target="_blank"
         >
-          <img src={linkedin} alt="" linkedin width="45px" />
+          <img src={linkedin} alt="linkedin" width="45px" />
         </a>
         <a href="https://github.com/SharifHafizi" target="_blank">
           <img src={github} alt="github" width="45px" />
@@ -20,6 +29,7 @@ function Footer() {
         <a href="https://www.instagram.com/sharifhafizi/" target="_blank">
           <img src={insta} alt="insta" width="45px" />
         </a>
+        <p>👁️{visitor}</p>
       </div>
     </footer>
   );
