89236ba5354fdbaa5646d8bc5420ff737a778a03
SharifHafizi <112721514+SharifHafizi@users.noreply.github.com>
2023-04-14 00:34:56 +0200

    link updated.

--- src/routes/projects/Projects.jsx
+++ src/routes/projects/Projects.jsx
@@ -235,7 +235,7 @@ function Projects() {
 
           <div className={style.buttons}>
             <a
-              href="https://sharifhafizi.github.io/weather-app/"
+              href="https://sharifhafizi.github.io/Weather-App/"
               target="_blank"
             >
               <button className={style.button1}>
