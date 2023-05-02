117efdda51b3ae1d5bef132cd78845710430a250
SharifHafizi <112721514+SharifHafizi@users.noreply.github.com>
2023-05-02 10:28:48 +0200

    file-path changed

--- src/routes/projects/projectData.js
+++ src/routes/projects/projectData.js
@@ -1,55 +1,64 @@
+import newYear from "./img-file/project-newyear.png";
+import weatherapp from "./img-file/weatherApp.png";
+import SSP from "./img-file/Solar-System-Project.png";
+import pokedex from "./img-file/Pokedex.png";
+import CSS3D from "./img-file/CSS-3D.png";
+import randompass from "./img-file/RandomPass.png";
+import converter from "./img-file/converter.png";
+import drumkit from "./img-file/DrumKitJS.png";
+
 export default [
   {
-    image: "src/routes/projects/img-file/project-newyear.png",
+    image: newYear,
     des: "New year counter project. This project were made with JS and React with some CSS.",
     live: "https://sharifhafizi.github.io/Christmas-Countdown/",
     github: "https://github.com/SharifHafizi/Christmas-Countdown.git",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/weatherApp.png",
+    image: weatherapp,
     des: "This Project is made with Vite, JS, Tailwind CSS and used API to fetch data for the weather forecast.",
     live: "https://sharifhafizi.github.io/Weather-App/",
     github: "https://github.com/SharifHafizi/Weather-App",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/Solar-System-Project.png",
+    image: SSP,
     des: "Solar system projetcs which is purely made just with HTML and CSS. It was my first project after learning html and css.",
     live: "https://sharifhafizi.github.io/space-project-main/",
     github: "https://github.com/SharifHafizi/space-project-main",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/Pokedex.png",
+    image: pokedex,
     des: "Pokedex projects is maded with JS and CSS. In this project we learned how to use API and fetch data.",
     live: "https://sharifhafizi.github.io/Api-Pokedex/",
     github: "https://github.com/SharifHafizi/Api-Pokedex",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/CSS-3D.png",
+    image: CSS3D,
     des: "This is a 3D image veiwer with animation which is purely made in CSS.",
     live: "https://sharifhafizi.github.io/CSS-3D/",
     github: "https://github.com/SharifHafizi/CSS-3D",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/RandomPass.png",
+    image: randompass,
     des: " This Project is made in JS and CSS which generate strong password for the user safety.",
     live: "https://sharifhafizi.github.io/Random-pass/",
     github: "https://github.com/SharifHafizi/Random-pass",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/converter.png",
+    image: converter,
     des: " This Project is made in JS and CSS which converts different unites.",
     live: "https://sharifhafizi.github.io/Unit-converter/",
     github: "https://github.com/SharifHafizi/Unit-converter",
     gitlogo: "src/routes/projects/img-file/githubSmal.png",
   },
   {
-    image: "src/routes/projects/img-file/DrumKitJS.png",
+    image: drumkit,
     des: "Drumkit Project which is purely made with javaScript. It works very fine and is ready to use as tablets or mobile apps. Internal CSS applied.",
     live: "https://sharifhafizi.github.io/Drum-Kit-JS/",
     github: "https://github.com/SharifHafizi/Drum-Kit-JS.git",
